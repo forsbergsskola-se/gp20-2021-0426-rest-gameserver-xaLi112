b310e1c6bc05f276c1c4b97615453e96baff3bd3
xaLi112 <74304902+xaLi112@users.noreply.github.com>
2021-04-28 17:39:33 +0200

    adds time server project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿// Using StreamReader and StreamWriter
+using System;
+using System.IO;
+using System.Net.Sockets;
 
-namespace TinyBrowser
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+public static class Program {
+    static void Main(string[] arguments) {
+        
+        // Connect to acme.com Server via TCP on HTTP-Default-Port 80.
+        var tcpClient = new TcpClient("acme.com", 80);
+        
+        // Send a HTTP-Request for the Root-Page
+        var streamWriter = new StreamWriter(tcpClient.GetStream());
+        streamWriter.Write("GET / HTTP/1.1\r\nHost: acme.com\r\n\r\n");
+        
+        // Read the Response
+        var streamReader = new StreamReader(tcpClient.GetStream());
+        var result = streamReader.ReadToEnd();
+        Console.WriteLine(result);
     }
 }
