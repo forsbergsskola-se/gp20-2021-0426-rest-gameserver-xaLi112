01f0de152826d26593428b3f1acc6c8f635b8e53
xaLi112 <74304902+xaLi112@users.noreply.github.com>
2021-05-06 16:41:00 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -20,4 +20,20 @@ public static class Program {
         tcpClient.Close();
         stream.Close();
     }
+    static string Navigator(string subDir) {
+        var tcpClient = new TcpClient();
+        var webSite = "www.acme.com";
+        tcpClient.Connect(webSite, 80);
+        var stream = tcpClient.GetStream();
+        var send = Encoding.ASCII.GetBytes($"GET /{subDir} HTTP/1.1" + Environment.NewLine +
+                                           "Host: acme.com" + Environment.NewLine+Environment.NewLine);
+        stream.Write(send, 0, send.Length);
+            
+        var sr = new StreamReader(stream);
+        var str = sr.ReadToEnd();
+        //Console.WriteLine(str);
+        tcpClient.Close();
+        stream.Close();
+        return str;
+    }
 }
